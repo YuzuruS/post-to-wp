73bb14a3b44fd2dac80679b3bd2bbf6579fce84f
YuzuruS <navitima@gmail.com>
2016-11-23 08:37:42 +0900

    update test

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -36,6 +36,35 @@ class PostTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($res['status']);
 	}
 
+	public function testWrongPost1()
+	{
+		$wp = new Post(getenv(WP_WRONGUSERNAME), getenv(WP_PASSWD), getenv(WP_ENDPOINT));
+
+		$res = $wp->makeCategories([
+			['name' => 'かて1', 'slug' => 'cate1'],
+			['name' => 'かて2', 'slug' => 'cate2'],
+		]);
+
+		$this->assertFalse($res['status']);
+	}
+
+	public function testWrongPost2()
+	{
+		$wp = new Post(getenv(WP_WRONGNAME), getenv(WP_PASSWD), getenv(WP_ENDPOINT));
+
+		$res = $wp
+			->setTitle('たいとる')
+			->setDescription('本文')
+			->setKeywords(['key1','key2'])
+			->setCategories(['かて1','かて2'])
+			->setDate('2016-11-11')
+			->setWpSlug('entry')
+			->setThumbnail('https://www.pakutaso.com/shared/img/thumb/SAYA160312500I9A3721_TP_V.jpg')
+			->post();
+
+		$this->assertFalse($res['status']);
+	}
+
 	public function tearDown()
 	{
 	}
